ce94e63359ceb8e1ebdf0fb250630f31d41e8b61
danivdwerf@gmail.com <danivdwerf@gmail.com>
2017-09-26 11:27:37 +0200

    created http request funtion for windows

--- includes/HTTP.cpp
+++ includes/HTTP.cpp
@@ -1,4 +1,5 @@
 #include "HTTP.h"
+#ifdef __APPLE__
 std::string HTTP::sendRequest(std::string url, std::map<std::string, std::string> headers, std::string values)
 {
   Poco::URI uri(url);
@@ -24,3 +25,48 @@ std::string HTTP::sendRequest(std::string url, std::map<std::string, std::string
   Poco::StreamCopier::copyStream(is, ss);
   return ss.str();
 }
+#endif
+
+#ifdef _WIN32
+std::string HTTP::sendRequest(int method, LPCSTR host, LPCSTR url, LPCSTR header, LPCSTR data)
+{
+    char httpUserAgent[512];
+    DWORD szhttpUserAgent = sizeof(httpUseragent);
+    ObtainUserAgentString(0, httpUseragent, &szhttpUserAgent);
+
+    char m[5];
+    if(method == GET)
+  		strcpy(m, "GET");
+  	else
+  		strcpy(m, "POST");
+
+    HINTERNET internet = InternetOpenA(httpUseragent, INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+    if(internet == NULL)
+    {
+      InternetCloseHandle(internet);
+      return NULL;
+    }
+
+    HINTERNET connect = InternetConnectA(internet, host, INTERNET_DEFAULT_HTTP_PORT, NULL, NULL, INTERNET_SERVICE_HTTP, 0, 0);
+    if(connect == NULL)
+    {
+      InternetCloseHandle(connect);
+      return NULL;
+    }
+
+    HINTERNET request = HttpOpenRequestA(connect, m, url, "HTTP/1.1", NULL, NULL, INTERNET_FLAG_HYPERLINK | INTERNET_FLAG_IGNORE_CERT_CN_INVALID | INTERNET_FLAG_IGNORE_CERT_DATE_INVALID | INTERNET_FLAG_IGNORE_REDIRECT_TO_HTTP  | INTERNET_FLAG_IGNORE_REDIRECT_TO_HTTPS | INTERNET_FLAG_NO_AUTH | INTERNET_FLAG_NO_CACHE_WRITE | INTERNET_FLAG_NO_UI | INTERNET_FLAG_PRAGMA_NOCACHE | INTERNET_FLAG_RELOAD, NULL);
+
+    if(request == NULL)
+      return NULL;
+
+    int datalen = 0;
+    if(data != NULL)
+      datalen = strlen(data);
+    int headerlen = 0;
+    if(header != NULL)
+      headerlen = strlen(header);
+
+    	HttpSendRequestA(request, header, headerlen, data, datalen);
+      InternetCloseHandle(request);
+}
+#endif
--- includes/HTTP.h
+++ includes/HTTP.h
@@ -1,13 +1,20 @@
 #ifndef HTTP_H
 #define HTTP_H
 
-#include <Poco/Net/HTTPClientSession.h>
-#include <Poco/Net/HTTPRequest.h>
-#include <Poco/Net/HTTPResponse.h>
-#include <Poco/StreamCopier.h>
-#include <Poco/Path.h>
-#include <Poco/URI.h>
-#include <Poco/Exception.h>
+#ifdef __APPLE__
+  #include <Poco/Net/HTTPClientSession.h>
+  #include <Poco/Net/HTTPRequest.h>
+  #include <Poco/Net/HTTPResponse.h>
+  #include <Poco/StreamCopier.h>
+  #include <Poco/Path.h>
+  #include <Poco/URI.h>
+  #include <Poco/Exception.h>
+#endif
+
+#ifdef _WIN32
+  #include <windows.h>
+  #include <wininet.h>
+#endif
 
 #include <sstream>
 #include <string>
@@ -15,6 +22,12 @@
 
 class HTTP
 {
-  public: std::string sendRequest(std::string, std::map<std::string, std::string>, std::string);
+  #ifdef __APPLE__
+    public: std::string sendRequest(std::string, std::map<std::string, std::string>, std::string);
+  #endif
+
+  #ifdef _WIN32
+    public: std::string sendRequest(int, LPCSTR, LPCSTR, LPCSTR, LPCSTR);
+  #endif
 };
 #endif
